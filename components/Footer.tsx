c1910b977ec5a1f0ddc6917e0295a2f126e9b531
mohdaizat34 <52664189+mohdaizat34@users.noreply.github.com>
2024-12-18 19:49:31 +0800

    change  to &apos;

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -17,7 +17,7 @@ const Footer = () => {
         </p>
         <a href="mailto:mohdaizat34@gmail.com">
           <MagicButton
-            title="Let's get in touch"
+            title="Lets get in touch"
             icon={<FaLocationArrow />}
             position="right"
           />
